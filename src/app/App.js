0f876b0fd4d1ece6461daec2ffe677633adac379
mflir <scavone.mf@gmail.com>
2019-04-16 00:05:21 -0300

    Weather app working

--- src/app/App.js
+++ src/app/App.js
@@ -22,19 +22,25 @@ class App extends Component {
     const cityValue = city.value;
     const countryValue = country.value;
 
-    const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`;
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    console.log(data);
+    if (cityValue && countryValue) {
 
-    this.setState({
-      temperature: data.main.temp,
-      description: data.weather[0].description,
-      wind_speed: data.wind.speed,
-      city: data.name,
-      country: data.sys.country,
-      error: null
-    }, () => console.log(this.state));
+      const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityValue},${countryValue}&appid=${WEATHER_KEY}&units=metric`;
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      console.log(data);
+
+      this.setState({
+        temperature: data.main.temp,
+        description: data.weather[0].description,
+        wind_speed: data.wind.speed,
+        city: data.name,
+        country: data.sys.country,
+        error: null
+      });
+
+    } else {
+      this.setState({ error: 'Please Enter a city and a country' })
+    }
   }
 
   render() {
--- src/app/components/WeatherInfo.js
+++ src/app/components/WeatherInfo.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
-const WeatherInfo = (props) => {
+const WeatherInfo = props => {
+  console.log(props);
   return (
-    <div>Weather Info</div>
+    <div>
+      {
+        props.error &&
+        <div className="alert alert-danger">
+          <p>{props.error}</p>
+        </div>
+      }
+      {props.temperature ?
+        <div className="card card-body">
+          <p>Location: {props.city} , {props.country}</p>
+          <p>Temperature: {props.temperature} C, {props.description}</p>
+          <p>Wind Speed: {props.wind_speed}</p>
+        </div>
+        :
+        <div className="card card-body">
+          <p> No Request yet </p>
+        </div>
+      }
+    </div>
   )
 }
 
